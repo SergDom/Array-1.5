3049a25835f17e7f6d8f1c6194d0787621e6663a
sergeydomanov <nomad-labs@yandex.ru>
2022-08-10 20:29:05 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+
+        int[] firstArr = new int[3];
+
+        double [] secondArr = new double[] {1.57,7.654, 9.986};
+
+
+
+
+
     }
 }
