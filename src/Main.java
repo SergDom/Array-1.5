a7c00d7369476faec36b356756ee471e5d43fd0c
sergeydomanov <nomad-labs@yandex.ru>
2022-08-11 20:58:53 +0300

    added branch

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         double[] secondArr = new double[] {1.57,7.654, 9.986};
 
         int [] thirdArr = new int[4];
+        thirdArr [0] = 2;
+        thirdArr [1] = 4;
+        thirdArr [2] = 8;
+        thirdArr [3] = 10;
 
         System.out.println();
         System.out.println("Задание 2");
@@ -15,16 +19,16 @@ public class Main {
         firstArr [1] = 2;
         firstArr [2] = 3;
         for (int i=0; i<3; i++) {
-        System.out.print(firstArr[i] + " ");
+        System.out.print(firstArr[i] + " , ");
         }
         System.out.println();
         for (int j=0; j<3; j++) {
-            System.out.print(secondArr[j] + " ");
+            System.out.print(secondArr[j] + " , ");
         }
         System.out.println();
 
-        for (int k=1; k<4; k++) {
-            System.out.print(thirdArr[k] + " ");
+        for (int k=0; k<4; k++) {
+           System.out.print(thirdArr[k] + " , ");
 
         }
 
@@ -37,7 +41,10 @@ public class Main {
         for (int j=secondArr.length-1; j>=0; j--) {
             System.out.print(secondArr[j] + " ");}
 
+        System.out.println();
+        System.out.println("Задание 4");
 
-
+        for (; i % 2 !=0; i++) {
+        System.out.print(firstArr[i] + " , ");}
     }
 }
